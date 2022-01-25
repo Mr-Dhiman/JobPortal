25f950ffa48ddf610259002eafd3e4b732f7a82e
Deepak Kumar <deepak.kumar@cyberoisystems.com>
2022-01-25 19:06:38 +0530

    updateDB

--- app.js
+++ app.js
@@ -1,39 +1,41 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const db = require('./config/db');
+const db = require("./config/db");
 const login_router = require("./router/login-router");
-const cors=require('cors');
+const cors = require("cors");
 app.use(cors());
 
-app.use(express.json());  // to parser request content_type:json
+app.use(express.json()); // to parser request content_type:json
 
 app.use(express.urlencoded({ extended: true })); // parse request content-type:form-data;
 
-
-
-
 // app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*"); 
+//     res.header("Access-Control-Allow-Origin", "*");
 //     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 //     next();
 //   });
 mongoose.Promise = global.Promise;
 
-app.use('/', login_router);
+app.use("/", login_router);
 
-mongoose.connect(db.url, {
-    useNewUrlParser: true
-}).then(() => {
+mongoose
+  .connect(db.url, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
     console.log("database connected");
-}).catch(err => {
+  })
+  .catch((err) => {
     console.log("connection Failed", err);
-})
+  });
 app.use(express.static("Hoitalent"));
-
 
 const port = process.env.PORT || 8082;
 
 app.listen(port, () => {
-    console.log("server is listening at port", port);
-})
+  console.log("server is listening at port", port);
+});
--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports={
-    url:'mongodb+srv://MrDhiman:<Mr.Dhiman1!>@hoitalent.czdsw.mongodb.net/Hoitalent?retryWrites=true&w=majority',
+    url:'mongodb+srv://Deepak07:Deepak07@main.5lbee.mongodb.net/JobPortal?retryWrites=true&w=majority',
     secret: "this-is-secretkey"
 }
